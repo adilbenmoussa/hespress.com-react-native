7d04ebd3d48a8e711b584520f72ac568298fc10b
Adil Ben Moussa <adil.benmoussa@agilexs.com>
2017-06-21 17:45:42 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+    SyleSheet,
+    Text
+} from 'react-native';
+
+class App extends Component {
+
+    render() {
+        return (
+            <Text> Salam Hesspress </Text>
+        );
+    }
+}
+
+export default App;
